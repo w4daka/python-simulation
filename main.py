f2bbf26634281dbb6e2a4df589ba350245cddf83
w4daka <213398204+w4daka@users.noreply.github.com>
2026-08-29 10:20:12 +0900

    chore:依存関係にdebuggpyを追加

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 def main():
     print("Hello from python-simulation!")
 
+def double(x):
+    result = x * 2
+    return result
+
+
+x = 10
+y = double(x)
+z = y + 5
+
+print(z)
+
 
 if __name__ == "__main__":
     main()
+    x = 10
+    y = x * 2
+    z = y + 5
+
+    print(z)
